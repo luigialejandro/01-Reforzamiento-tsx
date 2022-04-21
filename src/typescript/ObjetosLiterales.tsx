dcf9f3c5b91caeb45704efacc5d59e558c6848e3
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-04-20 23:16:10 -0500

    I've built two components: TiposBasicos.tsx which shows the right way of assigning types to our variables and ObjetosLiterales.tsx which shows the right way to write a literal object in TS and to deploy it in HTML

---
+++ src/typescript/ObjetosLiterales.tsx
@@ -0,0 +1,24 @@
+
+
+export const ObjetosLiterales = () => {
+
+    const persona = {
+        nombre: 'Fernando',
+        edad: 35,
+        direccion: {
+            pais: 'Canadá',
+            casaNo: 615
+        }
+    }
+
+  return (
+    <>
+        <h3>Objetos literales</h3>
+        <code>
+            <pre>
+                { JSON.stringify( persona, null, 2 ) }
+            </pre>
+        </code>
+    </>
+  )
+}
--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,9 +1,19 @@
 
 
 export const TiposBasicos = () => {
+
+  const nombre: string = 'Fernando';
+  const edad: number = 35;
+  const estaActivo: boolean = false;
+
+  const poderes: string[] = []; //'Velocidad', 'Volar', 'Respirar en el agua'
+
   return (
     <>
     <h3>Tipos básicos</h3>
+    { nombre }, { edad }, { (estaActivo) ? 'activo' : 'no activo' }
+    <br/>
+    { poderes.join(', ') }
     </>
   )
 }
